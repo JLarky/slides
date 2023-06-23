486808b2789e676b01565e5e6a9de05fa93a0482
JLarky <jlarky@gmail.com>
2023-06-23 09:20:33 -0600

    tried if (0)
    https://twitter.com/TheLarkInn/status/1672257287052984320

--- 2023-07-rscs/demos/next-app/src/app/page.tsx
+++ 2023-07-rscs/demos/next-app/src/app/page.tsx
@@ -5,10 +5,12 @@ import { HeavyComponent } from "~/ui/HeavyComponent";
 export const dynamic = "force-dynamic";
 
 export default function Home() {
+  if (0) {
+    return <HeavyComponent />;
+  }
   return (
     <div>
       <h1>Next.js</h1>
-      {0 && <HeavyComponent />}
     </div>
   );
 }
