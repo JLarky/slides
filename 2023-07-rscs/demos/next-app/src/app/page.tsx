5f27a14652e9c3902d48efadb6bb704aeb4a87a0
JLarky <jlarky@gmail.com>
2023-06-23 09:33:52 -0600

    second example
    - https://twitter.com/JLarky/status/1672265461969321985

--- 2023-07-rscs/demos/next-app/src/app/page.tsx
+++ 2023-07-rscs/demos/next-app/src/app/page.tsx
@@ -1,16 +1,17 @@
 "use client";
 
+import React from "react";
 import { HeavyComponent } from "~/ui/HeavyComponent";
 
 export const dynamic = "force-dynamic";
 
+const C = false ? HeavyComponent : React.Fragment;
+
 export default function Home() {
-  if (0) {
-    return <HeavyComponent />;
-  }
   return (
     <div>
       <h1>Next.js</h1>
+      <C />
     </div>
   );
 }
