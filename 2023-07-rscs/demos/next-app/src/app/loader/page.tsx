2865db544d701aa0f0f94c8a88ae18c0b0a429da
JLarky <jlarky@gmail.com>
2023-06-24 18:45:59 -0600

    next loader

---
+++ 2023-07-rscs/demos/next-app/src/app/loader/page.tsx
@@ -0,0 +1,12 @@
+import { Loader } from "~/ui/Loader";
+
+export const dynamic = "force-dynamic";
+
+export default function Home() {
+  return (
+    <div>
+      123
+      <Loader />
+    </div>
+  );
+}
---
+++ 2023-07-rscs/demos/next-app/src/ui/Loader.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export const loader = () => {
+  console.log("fetching data");
+  return fetch("https://api.github.com/repos/fogbender/b2b-saaskit")
+    .then((res) => res.json() as Promise<{ stargazers_count: number }>)
+    .then((data) => data.stargazers_count);
+};
+export const Loader = async () => {
+  const data = await loader();
+  return <div>Loader {data}</div>;
+};
