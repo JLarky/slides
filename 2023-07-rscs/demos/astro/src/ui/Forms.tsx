28159b9eecb4809bf4bfb12368cf13ec568bbfa8
JLarky <jlarky@gmail.com>
2023-07-01 22:19:28 -0600

    WIP

---
+++ 2023-07-rscs/demos/astro/src/ui/Forms.tsx
@@ -0,0 +1,36 @@
+import type {} from 'react/experimental';
+import { experimental_useFormStatus as useFormStatus } from 'react-dom';
+import type {} from 'react-dom/experimental';
+
+const addRecord = async function addRecord(formData: FormData) {
+	await new Promise((resolve) => setTimeout(resolve, 300));
+	for (const [key, value] of formData) {
+		console.log(key, value);
+	}
+};
+
+export const Forms = () => {
+	return (
+		<form
+			action={async (formData: FormData) => {
+				console.log('start transition');
+				await addRecord(formData);
+				console.log('end transition');
+			}}
+		>
+			<input type="text" name="name" />
+			<Submit />
+			<Submit2 />
+		</form>
+	);
+};
+
+const Submit = () => {
+	const { pending } = useFormStatus();
+	return <input name="submit" type="submit" value="Submit" disabled={pending} />;
+};
+
+const Submit2 = () => {
+	const { pending } = useFormStatus();
+	return <input name="submit2" type="submit" value="Submit2" disabled={pending} />;
+};
---
+++ 2023-07-rscs/demos/astro/src/ui/LoaderCache.tsx
@@ -0,0 +1,34 @@
+'use client';
+import { cache } from 'react';
+// ReactFeatureFlags = require('shared/ReactFeatureFlags');
+// import ReactFeatureFlags from 'shared/ReactFeatureFlags';
+
+// ReactFeatureFlags.enableCache = true;
+
+console.log(cache);
+
+export const loader = cache(async () => {
+	await new Promise((resolve) => setTimeout(resolve, 1000));
+	console.log('fetching data');
+	// const res = await fetch('https://api.github.com/repos/fogbender/b2b-saaskit');
+	// return (await res.json()).stargazers_count;
+});
+
+const test = cache(async () => 123);
+
+export const LoaderCache = async () => {
+	console.log(test());
+	// const data = await loader();
+	return (
+		<>
+			data
+			<Loader />
+		</>
+	);
+};
+
+export const Loader = async () => {
+	// const data = await loader();
+	const data = 123;
+	return <div>Loader {data}</div>;
+};
